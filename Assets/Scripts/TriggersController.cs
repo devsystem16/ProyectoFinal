9f7b3d1d99fdfcf8c541b8a15d46db7fc5ada165
mcastro <desarrollos16@hotmail.com>
2017-09-25 18:14:48 -0500

    camaras

--- Assets/Scripts/TriggersController.cs
+++ Assets/Scripts/TriggersController.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class TriggersController : MonoBehaviour {
 
- 
 
+
+    public GameObject camara1;
+    public GameObject camara2;
+    public GameObject camara3;
 
     void OnTriggerEnter(Collider collider)
     {
         Debug.Log(collider.gameObject.name);
+        camara2.SetActive(true);
+        camara1.SetActive(false);
+
     }
+
+
+    void OnTriggerExit(Collider collider) {
+
+        camara2.SetActive(false);
+        camara1.SetActive(true);
+    }
+
 }
