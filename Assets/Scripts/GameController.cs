eedc756256df8e9127d761349a1796e74338c0d1
mcastro <desarrollos16@hotmail.com>
2017-09-25 17:37:59 -0500

    Carga  proyecto

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+
+    public GameObject autobus;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            autobus.GetComponent<Transform>().transform.Translate(Vector3.forward * 3);
+
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            autobus.GetComponent<Transform>().transform.Translate(Vector3.back *3);
+
+    }
+
+
+  
+}
---
+++ Assets/Scripts/TriggersController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggersController : MonoBehaviour {
+
+ 
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log(collider.gameObject.name);
+    }
+}
