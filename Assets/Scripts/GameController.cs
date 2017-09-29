2a7177b59bd81e902a3f0e1d2d02afcde4cece68
mcastro <desarrollos16@hotmail.com>
2017-09-29 17:50:32 -0500

    2

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,9 +6,11 @@ public class GameController : MonoBehaviour {
 
 
     public GameObject autobus;
-	// Use this for initialization
+    public GUISkin miGUI;
+    private bool pausado; 
+
 	void Start () {
-		
+        pausado = false;
 	}
 
     // Update is called once per frame
@@ -16,14 +18,37 @@ public class GameController : MonoBehaviour {
 
         if (Input.GetKey(KeyCode.UpArrow))
             autobus.GetComponent<Transform>().transform.Translate(Vector3.forward * 1);
-
-  
-
+ 
         if (Input.GetKey(KeyCode.DownArrow))
             autobus.GetComponent<Transform>().transform.Translate(Vector3.back *1);
 
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+            pausado = (pausado == true) ? false : true ;
     }
 
+    public int posicionY = 400;
+    public int posicionX = 600;
 
+    void OnGUI() {
+
+        if (miGUI)
+            GUI.skin = miGUI;
+
+        if (pausado)
+        {
+
+          
+            int resultX = (Screen.width * 660) / 1266;
+            int resultY = (Screen.height * 560) / 931;
+            GUI.Box(new Rect(Screen.width / 2 - 380, Screen.height / 2 - 250,  (Screen.width -(Screen.width - resultX)) ,  
+            (Screen.height - (Screen.height - resultY))    ), "Información Gestual" );
+ 
+
+            GUI.Label(new Rect(Screen.width / 2 - 70, Screen.height / 2 , 140, 30), "Cantidad de salud");
+
+        }
+
+
+    }
   
 }
